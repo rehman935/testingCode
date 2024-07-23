b1c4cacdbb409e8e1d762aaeafd283ee4cad5959
nouman75 <carcultus79@gmail.com>
2024-07-24 00:45:55 +0100

    Update Config.py this file
    Update Config.py this file

--- Config.py
+++ Config.py
@@ -49,3 +49,147 @@ class Config:
     TYPE_COLS = ['y2', 'y3', 'y4']
     CLASS_COL = 'y2'
     GROUPED = 'y1'
+
+
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+
+
+
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+
+
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+
+# Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
+    
