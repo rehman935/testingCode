65136ae1b958498aeebf95263a95aeb09631d10a
nouman75 <carcultus79@gmail.com>
2024-07-24 00:55:19 +0100

    Update Config.py Nouman

--- Config.py
+++ Config.py
@@ -3,6 +3,7 @@ class Config:
     TICKET_SUMMARY = 'Ticket Summary'
     INTERACTION_CONTENT = 'Interaction content'
 
+Nouman ali
     # Type Columns to test
     TYPE_COLS = ['y2', 'y3', 'y4']
     CLASS_COL = 'y2'
